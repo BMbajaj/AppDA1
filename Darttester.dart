2427b6526645f0541203eb7a1ce2bb7898a94df6
BHAVYA BAJAJ <144222539+BMbajaj@users.noreply.github.com>
2023-11-28 01:47:01 +0530

    Add files via upload

--- Darttester.dart
+++ Darttester.dart
@@ -1,52 +1,3 @@
-// import 'dart:svg';
-
-// void main() {
-//   //for (int i = 0; i < 5; i++) {
-//    // print('hello ${i + 1}');
-//     print('Everbody is a ${f1('Ferrari')} fan');
-//     print(f2());
-//     User mathsic= User('Amit Setia',29);
-//     mathsic.login();
-//     Superuser mathsic2= Superuser('Yoshi',30);
-//     mathsic2.login();
-//     mathsic2.publish();
-//     print(mathsic2.username);
-//   //}
-// }
-// String f1(Merc){
-//   Merc='Mercedes';
-//   return Merc;
-// }
-// List f2(){
-//   List name=['122',123];
-//   name.add(3);
-//   return name;
-// }
-// class User {
-  
-//   late String username;
-//   late int age;
-  
-//   User(String user, int age){    //constructor
-//       username=user;
-//       age=age;                   //code already knows you're mentioning current instance of class therefore mentioning this is not required
-//   }
-//   void login(){
-//     if (username=="Amit Setia"){
-//       print("User logged in");
-//     }else{
-//       print("Invalid User");
-//     }
-//   }
-// }
-
-// class Superuser extends User{//assigning variable names to a different variable, why is variable name required to be the same
-//     Superuser(String abc, int age) : super(abc, age);//constructor for the superuser//values aren't set here, it inherits values from the user class itself 
-//     void publish(){
-//           print('${username} has been published');
-//           print("wowzers");
-//     }
-// }
 
 List clist=[1,2,".",3,"+",3,4,5,"+",4];
 List dlist=[];
@@ -123,11 +74,4 @@ void main(){
   }
   print(dlist);
   }
-  // while(clist[i] =="+"|| clist[i]=="*"|| clist[i]=="/"|| clist[i]=="-"){
-  //   dlist.add(clist[i]);
-  //   i++;
-  //   print(dlist);
-  // }
-// int j=0;
-// while (j<dlist.length;j++ ){
-  }
+}
